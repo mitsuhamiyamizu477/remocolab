f2c0109c7d53ebb33a9409db97ac6bab734e6931
mitsuhamiyamizu477 <100654798+mitsuhamiyamizu477@users.noreply.github.com>
2022-04-04 14:16:03 +0800

    Update remocolab.py

--- remocolab.py
+++ remocolab.py
@@ -365,7 +365,7 @@ def _setupVNC():
   my_apt.installDebPackage("virtualgl.deb")
   my_apt.installDebPackage("turbovnc.deb")
 
-  my_apt.installPkg("icewm", "lxterminal", "nautilus", "htop", "firefox")
+  my_apt.installPkg("icewm", "lxterminal", "nautilus", "htop", "firefox", "figlet")
   my_apt.commit()
   my_apt.close()
 
